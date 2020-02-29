59a63006121a8538297c09e694bbc9a8230e736a
Gleb <gleb.beloded@gmail.com>
2020-02-29 18:00:42 +0300

    transmission daemon support added

--- bootstrap.sh
+++ bootstrap.sh
@@ -37,3 +37,20 @@ cp .zshrc ~
 cp -r i3 ~/.config
 
 usermod --shell /usr/bin/zsh $USER
+
+#torrent client
+dnf install transmission-daemon
+#stop it in order to substitute config files
+systemctl stop transmission-daemon
+systemctl disable transmission-daemon.service 
+
+#change folder to which transmission downloads files
+sed -i
+sed -i -E "s/(.*\"download-dir\": \"\/home\/)(.*)(\/Downloads\",$)/\1$USER\3/" ./settings.json
+cp transmission/settings.json /var/lib/transmission/.config/transmission-daemon/
+#give service user permissions so it can write to ~/Downloads
+sed -i "s/User=.*$/User=$USER/" ./transmission/transmission-daemon.service
+cp ./transmission/transmission-daemon.service /usr/lib/systemd/system/transmission-daemon.service
+#restart the daemon
+systemctl enable transmission-daemon.service 
+systemctl start transmission-daemon
