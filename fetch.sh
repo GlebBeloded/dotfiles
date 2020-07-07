484c49a8d46c49ea05c0484d3c511b0fb7b3668e
Gleb Beloded <gleb.beloded@gmail.com>
2020-07-07 22:40:20 +0300

    xdg open stuff

--- fetch.sh
+++ fetch.sh
@@ -4,25 +4,31 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # xserver config
-/bin/cp /home/gleb/{.xinitrc,.xprofile,.Xresources} $DIR
+/bin/cp ~/{.xinitrc,.xprofile,.Xresources} $DIR
+
 # i3 config
-/bin/cp /home/gleb/.config/i3/config $DIR/config/i3/
+/bin/cp ~/.config/i3/config $DIR/config/i3/
+
 # wallpaper that I use
-/bin/cp /home/gleb/.config/wallpaper $DIR/config/
+/bin/cp ~/.config/wallpaper $DIR/config/
+
 # compton config
-/bin/cp /home/gleb/.config/compton.conf $DIR/config/
+/bin/cp ~/.config/compton.conf $DIR/config/
+
 # polybar config
-/bin/cp -r /home/gleb/.config/polybar/ $DIR/config/
+/bin/cp -r ~/.config/polybar/ $DIR/config/
+
 # vscode stuff
-/bin/cp -r /home/gleb/.config/Code/User/keybindings.json $DIR/config/Code/keybindings.json
-/bin/cp -r /home/gleb/.config/Code/User/settings.json $DIR/config/Code/settings.json
+/bin/cp -r ~/.config/Code/User/keybindings.json $DIR/config/Code/keybindings.json
+/bin/cp -r ~/.config/Code/User/settings.json $DIR/config/Code/settings.json
 code --list-extensions > $DIR/config/Code/extensions
+
 # rss reader
-/bin/cp -r /home/gleb/.newsboat/{urls,config} $DIR/config/newsboat
+/bin/cp -r ~/.newsboat/{urls,config} $DIR/config/newsboat
 
 # zsh stuff
-/bin/cp -r /home/gleb/{.zprofile,.zshrc} $DIR
-/bin/cp -r /home/gleb/.config/zsh/aliasrc $DIR/config/zsh/
+/bin/cp -r ~/{.zprofile,.zshrc} $DIR
+/bin/cp -r ~/.config/zsh/aliasrc $DIR/config/zsh/
 # update zsh auto-completion scripts
 git clone https://github.com/zsh-users/zsh-completions.git /tmp/zsh-completions
 cp -r /tmp/zsh-completions/src/{_cmake,_cppcheck,_golang,_openssl,_xinput} $DIR/config/zsh/completions
@@ -31,4 +37,7 @@ rustup update
 rustup completions zsh > $DIR/config/zsh/completions/_rust
 
 # lf config and scripts
-cp -r /home/gleb/.config/lf $DIR/config/
+cp -r ~/.config/lf $DIR/config/
+
+# xdg mime-open
+cp  ~/.config/mimeapps.list $DIR/config/
