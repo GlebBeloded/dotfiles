5005799eadd96fcdf9b31cb3b2b04a74edb9f1bc
Gleb Beloded <gleb.beloded@gmail.com>
2020-07-08 19:04:43 +0300

    script permission fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,102 +9,129 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # export DIR variable for install script
 export DIR
 
-# User space installs
-if [ $EUID != 0 ]; then
-  # top bar
-  $DIR/install.sh https://aur.archlinux.org/polybar.git /tmp/polybar
-  # bunch of fonts, weights alot, probably must be replcaed
-  $DIR/install.sh https://aur.archlinux.org/nerd-fonts-complete.git /tmp/nerd
-  # top replacement
-  $DIR/install.sh https://aur.archlinux.org/ytop.git /tmp/ytop
-fi
-
-# if not root get root rights and rerun the script with usr and usrhome enviroment variable 
+# run script as root first, then run it as normal user
 if [ $EUID != 0 ]; then
   USRHOME=${HOME}
   _USER=${USER}
   export USRHOME
   export _USER
   sudo -E "$0" "$@"
-  exit $?
-fi
-
-# Ensure ~/.local exists
-if [ ! -d $USRHOME/.local ];then
-  mkdir $USRHOME/.local
-  mkdir $USRHOME/.local/bin
-  mkdir $USRHOME/.local/share
-fi
-
-# Ensure ~/.config exists
-if [ ! -d $USRHOME/.config ];then
-  mkdir $USRHOME/.config
 fi
  
-# Ensure the base-devel package group is installed in full 
-pacman -S --needed base-devel --noconfirm
-pacman -Syu --noconfirm
+# installs which require root priveleges
+if [ $EUID == 0 ]; then
+  # Ensure the base-devel package group is installed in full 
+  pacman -S --needed base-devel --noconfirm
+  pacman -Syu --noconfirm
 
-# Install Desktop environment stuff
-pacman -S --noconfirm i3-wm unclutter picom rofi pulseaudio
+  # Install Desktop environment stuff
+  pacman -S --noconfirm i3-wm unclutter picom rofi pulseaudio
 
-# Install programming stuff
-# C, C++, Python, Go, Rust
-pacman -S --noconfirm gcc make cmake python python-pip go rust
-# Miscallenious stuff
-pacman -S --noconfirm neovim zsh jq code docker git
-pacman -S --noconfirm --asdeps xclip xsel
+  # Install programming stuff
+  # C, C++, Python, Go, Rust
+  pacman -S --noconfirm gcc make cmake python python-pip go rust
+  
+  # Miscallenious stuff
+  pacman -S --noconfirm neovim zsh jq code docker git man
+  pacman -S --noconfirm --asdeps xclip xsel
 
-# Install browser
-pacman -S --noconfirm firefox
-# install all firefox optional dependencies to get media codecs
-pacman -S --asdeps --noconfirm hunspell-en_US 
-# Install media stuff
-pacman -S --noconfirm feh mpv newsboat zathura zathura-pdf-mupdf zathura-djvu
+  # Install browser
+  pacman -S --noconfirm firefox
+  # install all firefox optional dependencies to get media codecs
+  pacman -S --asdeps --noconfirm hunspell-en_US 
+  
+  # Install media stuff
+  pacman -S --noconfirm feh mpv newsboat zathura zathura-pdf-mupdf zathura-djvu
 
-# install swallow script for i3
-git clone https://github.com/jamesofarrell/i3-swallow.git /tmp/swallow
-/usr/bin/cp /tmp/swallow/swallow $USRHOME/.local/bin/
+  # dependencies for image/video previews in lf
+  pacman -S --noconfirm poppler ffmpegthumbnailer w3m imagemagick
 
-#install vscode extensions
-while read ext; do
-  code --install-extension $ext
-done < $DIR/config/Code/extensions
+  # simple terminal
+  git clone https://github.com/GlebBeloded/st.git /tmp/st
+  cd /tmp/st
+  make install
+  cd $DIR
 
-simple terminal
-git clone https://github.com/GlebBeloded/st.git /tmp/st
-cd /tmp/st
-make install
-cd $DIR
+  # zsh syntax highlighting
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /tmp/zsh-syntax
+  cd /tmp/zsh-syntax && make install
+  cd $DIR
 
-# zsh related stuff
-# move zshrc and zprofile home 
-/usr/bin/cp $DIR/{.zprofile,.zshrc} $USRHOME
-# also move .zshrc to root home, because I want root prompt to be the same as main prompt
-/usr/bin/cp $DIR/.zshrc /root/
-#make zshell default
-usermod --shell /usr/bin/zsh $_USER
-# zsh history file
-mkdir -p $USERHOME/.cache/zsh
-touch $USERHOME/.cache/zsh/history
-# zsh syntax highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /tmp/zsh-syntax
-cd /tmp/zsh-syntax && make install
-cd $DIR
+  # make zsh default
+  usermod --shell /usr/bin/zsh $_USER
+  usermod --shell /usr/bin/zsh $USER
 
-# lf and stuff which has to do with image previews
-# install lf
-git clone https://github.com/Provessor/lf.git /tmp/lf
-cd /tmp/lf
-go build -o $USRHOME/.local/bin/lf
-cd $DIR
-# dependencies for image/video previews in lf
-pacman -S --noconfirm poppler ffmpegthumbnailer w3m imagemagick
-pip3 install ueberzug Pillow
+  # move .zshrc to root home, because I want root prompt to be the same as main prompt
+  /usr/bin/cp $DIR/.zshrc /root/
+fi
 
-# move dotfiles
-/usr/bin/cp $DIR/{.Xresources,.xprofile,.xinitrc} $USRHOME
-/usr/bin/cp -r $DIR/config/* $USRHOME/.config/
+# make sure all neccessary directories exist
+if [ $EUID != 0 ];then 
+  # Ensure ~/.local exists
+  if [ ! -d ~/.local ];then
+    mkdir ~/.local
+    if [ ! -d ~/.local/bin ];then
+        mkdir ~/.local/bin
+    fi
+    if [ ! -d ~/.local/share ];then
+      mkdir ~/.local/share
+    fi
+  fi
 
-# move some programs and configs to ~/.local
-/usr/bin/cp -r $DIR/local/* $USRHOME/.local/
+  # Ensure ~/.config exists
+  if [ ! -d ~/.config ];then
+    mkdir ~/.config
+  fi
+
+  if [ ! -d ~/.cache ];then
+    mkdir ~/.cache
+    if [ ! -d ~/.chache/zsh ];then
+      mkdir ~/.cache/zsh
+    fi
+  fi
+fi
+
+# User space installs
+if [ $EUID != 0 ]; then
+  # top bar
+  $DIR/aur_install.sh https://aur.archlinux.org/polybar.git /tmp/polybar
+  # bunch of fonts, weights alot, probably must be replcaed
+  $DIR/aur_install.sh https://aur.archlinux.org/nerd-fonts-complete.git /tmp/nerd
+  # top replacement
+  $DIR/aur_install.sh https://aur.archlinux.org/ytop.git /tmp/ytop
+
+  # install swallow script for i3
+  git clone https://github.com/jamesofarrell/i3-swallow.git /tmp/swallow
+  /usr/bin/cp /tmp/swallow/swallow ~/.local/bin/
+
+  #install vscode extensions
+  while read ext; do
+    code --install-extension $ext
+  done < $DIR/config/Code/extensions
+
+  # lf and stuff which has to do with image previews
+  # install lf
+  git clone https://github.com/Provessor/lf.git /tmp/lf
+  cd /tmp/lf
+  go build -o ~/.local/bin/lf
+  cd $DIR
+  pip3 install ueberzug Pillow
+
+  # zsh history file
+  touch ~/.cache/zsh/history
+fi
+
+# move the actual dotfiles
+if [ $EUID != 0 ];then
+  # zsh related stuff
+  # move zshrc and zprofile home 
+  /usr/bin/cp $DIR/{.zprofile,.zshrc} ~
+
+  # move dotfiles
+  /usr/bin/cp $DIR/{.Xresources,.xprofile,.xinitrc} ~
+  /usr/bin/cp -r $DIR/config/* ~/.config/
+
+  # move some programs and configs to ~/.local
+  /usr/bin/cp -r $DIR/local/bin/* ~/.local/bin/
+  /usr/bin/cp -r $DIR/local/share/* ~/.local/share/
+fi
